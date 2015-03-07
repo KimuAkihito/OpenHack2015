c1600e102c4a3f62f405137701aac129cff375a5
root <root@upallnight.jp>
2015-03-08 07:04:40 +0900

    もでるじっそう

--- fuel/app/classes/model/wishlist.php
+++ fuel/app/classes/model/wishlist.php
@@ -1,23 +1,31 @@
 <?php
 namespace Model;
-
+use \DB;
 class WishList extends \Model  {
     public static function get_all()
     {
-        $results = \DB::query('SELECT * FROM wish_list')->execute();
+        $query = DB::query('SELECT * FROM wish_list');
+        $results = $query->execute();
         return $results->as_array();
     }
 
+    public static function get($id) {
+      $query = DB::query('SELECT * FROM wish_list WHERE id = `$id`');
+      $results = $query->execute();
+      return $results->as_array();
+    }
+
     public static function insert($id) {
-      $query = \DB::insert('wish_list');
+      $query = DB::insert('wish_list');
       $query->set(array(
-        "id" => "$id",
+        "id" => $id,
         "flag" => false
       ));
+      $query->execute();
     }
 
     public static function update($id) {
-      $query = \DB::update('wish_list');
+      $query = DB::update('wish_list');
       $query->where('id', '=', '$id');
       $query->value('flag', true);
     }
