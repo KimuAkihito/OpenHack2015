ae4f2e93d8484af3494213a019865c53f24810c6
root <root@upallnight.jp>
2015-03-07 23:26:49 +0900

    gitignoreの修正

---
+++ fuel/app/config/routes.php
@@ -0,0 +1,8 @@
+<?php
+return array(
+	'_root_'  => 'login/index',  // The default route
+	'_404_'   => 'welcome/404',    // The main 404 route
+
+    'login/auth' => 'login/auth',
+    'home/index' => 'home/index',
+);
