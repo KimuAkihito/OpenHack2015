0923fa0e32a8bfce0c4ad3dbc10c4c7caef70c58
kouki.higashikawa <higashikawa.kouki@headwaters.co.jp>
2015-03-08 05:24:29 +0900

    recommit

--- fuel/app/config/db.php
+++ fuel/app/config/db.php
@@ -6,30 +6,11 @@
  */
 
 return array(
-<<<<<<< HEAD
-    'default' => array(
-        'type'        => 'pdo',
-        'connection'  => array(
-            'dsn'        => 'mysql:host=localhost;dbname=openhack;charset=utf8;unix_socket=/private/tmp/mysql.sock',
-            'username'   => 'root',
-            'password'   => '7102531k',
-            'persistent' => false,
-            'compress'   => false,
-        ),
-        'identifier'   => '',
-        'table_prefix' => '',
-        'charset'      => 'utf8',
-        'enable_cache' => true,
-        'profiling'    => false,
+   'default' => array(
+      'connection'  => array(
+          'dsn'        => 'mysql:host=localhost;dbname=openhackday',
+          'username'   => 'root',
+          'password'   => '',
+      ),
     ),
 );
-=======
-  'default' => array(
-      'connection'  => array(
-      'dsn'        => 'mysql:host=localhost;dbname=openhackday',
-      'username'   => 'root',
-      'password'   => '',
-    ),
-  ),
-)
->>>>>>> 1d94572be18a2b834df33916884e2c554e5cb3c5
